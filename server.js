87fdb14403564310d5d290673d83f34a5b48f84a
danielsoaresti <danielsoares.ti@gmail.com>
2016-05-02 15:28:04 -0400

    Change confis.

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  //res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
